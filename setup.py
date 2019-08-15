0326e31e33f2da626d1590195c3da52eafd2935a
Nicholas Long <nicholas.long@nrel.gov>
2019-08-15 12:24:54 -0600

    run python setup.py build to import portions of mbl

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from setuptools import setup, find_packages
 
-from management.update_schemas import UpdateSchemas
 from management.update_licenses import UpdateLicenses
+from management.update_schemas import UpdateSchemas
 
 with open('README.rst') as f:
     readme = f.read()
@@ -57,3 +57,37 @@ setup(
         'update_licenses': UpdateLicenses,
     },
 )
+
+# install the Modelica Buildings Library
+from zipfile import ZipFile
+from io import BytesIO
+from requests import get
+import os
+import shutil
+
+libs_to_extract = ['Buildings/Applications/DHC']
+save_path = 'geojson_modelica_translator/modelica/buildingslibrary'
+tmp_save_path = 'geojson_modelica_translator/modelica/tmp_buildingslibrary'
+repo_name = 'modelica-buildings'
+if os.path.exists(save_path):
+    shutil.rmtree(save_path)
+if os.path.exists(tmp_save_path):
+    shutil.rmtree(tmp_save_path)
+mbl_archive_name = 'issue1442_loadCoupling'
+r = get(f'https://github.com/lbl-srg/{repo_name}/archive/{mbl_archive_name}.zip')
+with ZipFile(BytesIO(r.content)) as zip:
+    files = zip.namelist()
+    for file in files:
+        # check if this needs to be extracted by looking into the libs_to_extract list
+        for lib_to_extract in libs_to_extract:
+            # make the path system independent when searching
+            if os.path.join(lib_to_extract.replace('/', os.path.sep)) in file:
+                print(f'extracting ... {file}')
+                zip.extract(file, path=tmp_save_path)
+
+# Move the whole directory
+shutil.move(os.path.join(tmp_save_path, f'{repo_name}-{mbl_archive_name}'), save_path)
+if os.path.exists(tmp_save_path):
+    shutil.rmtree(tmp_save_path)
+# z.extract('open-location-code-68ba7ed4c6e7fae41a0255e4394ba1fa0f8435bb/python/*',
+#           path=)
