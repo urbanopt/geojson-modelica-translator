3143a52a991fe0f17f633c0c757273ed5f902d70
goldcoder01 <yanfei.li@nrel.gov>
2019-12-11 16:54:58 -0700

    add testcase for ets_template

---
+++ tests/model_connectors/test_ets.py
@@ -0,0 +1,35 @@
+from ..context import geojson_modelica_translator  # noqa - Do not remove this line
+
+import unittest
+from geojson_modelica_translator.model_connectors.ets_template import ETS_Template
+
+
+class ETS_ModelConnectorSingleBuildingTest(unittest.TestCase):
+    def setUp(self): # the first method/member must be setUp
+        self.thermal_junction_properties_geojson = "/home/mindcoder/geojson-modelica-translator/geojson_modelica_translator/geojson/data/schemas/thermal_junction_properties.json"
+        self.system_parameters_geojson = "/home/mindcoder/geojson-modelica-translator/geojson_modelica_translator/system_parameters/schema.json"
+        self.ets_from_building_modelica = "/home/mindcoder/geojson-modelica-translator/geojson_modelica_translator/modelica/buildingslibrary/Buildings/Applications/DHC/EnergyTransferStations/CoolingIndirect.mo"
+
+        self.ets = ETS_Template(self.thermal_junction_properties_geojson, self.system_parameters_geojson, self.ets_from_building_modelica)
+        self.assertIsNotNone(self.ets)
+
+        return self.ets
+
+
+    def test_ets_thermal_junction(self):
+        ets_general = self.ets.check_ets_thermal_junction()
+        #print ("yanfei: ", ets_general)
+        self.assertTrue( ets_general )
+
+    def test_ets_system_parameters(self):
+        self.assertTrue( self.ets.check_system_parameters() )
+
+    def test_ets_from_building_modelica(self):
+        self.assertTrue( self.ets.check_ets_from_building_modelica() )
+
+    def test_ets_to_modelica(self):
+        self.assertTrue( self.ets.to_modelica() )
+
+
+if __name__ == '__main__':
+    unittest.main()
