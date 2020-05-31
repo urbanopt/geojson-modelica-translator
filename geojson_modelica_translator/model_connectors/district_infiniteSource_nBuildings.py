ae4fcac51e4e1006a15e951e9ec0a4d4a3286321
YanfeiNREL <Yanfei.Li@nrel.gov>
2020-05-31 10:52:20 -0700

    revise for district_nBuildings_infinite

--- geojson_modelica_translator/model_connectors/district_infiniteSource_nBuildings.py
+++ geojson_modelica_translator/model_connectors/district_infiniteSource_nBuildings.py
@@ -1,51 +1,158 @@
 # import json
 import os
 
+from geojson_modelica_translator.model_connectors.base import \
+    Base as model_connector_base
+from geojson_modelica_translator.modelica.input_parser import PackageParser
+from geojson_modelica_translator.utils import ModelicaPath
 from jinja2 import Environment, FileSystemLoader
 
 
-class DistrictInfiniteSourceNBuildingsTemplate:
-    """This class will generate a district model with infinite source for
-    arbitrary number of buildings connected with ETS"""
-    def __init__(self, nBuilding):
-        super().__init__()
-
-        self.nBuilding = nBuilding
-        directory_up_one_level = os.path.abspath(os.path.join(__file__, "../../.."))
-        self.directory_district_nBuildings = os.path.join(
-            directory_up_one_level + "/tests/output/district_nBuildings/"
-        )
-        if not os.path.exists(self.directory_district_nBuildings):
-            os.makedirs(self.directory_district_nBuildings)
+class DistrictInfiniteSourceNBuildingsTemplate(model_connector_base):
+    """This class will template a district with arbitrary number of buildings,
+    connected with ets and infinite-cooling.
+    """
+    def __init__(self, system_parameters):
+        super().__init__(system_parameters)
 
         # here comes the Jinja2 function: Environment()
         # it loads all the "*.mot" files into an environment by Jinja2
-        self.template_env = Environment(
-            loader=FileSystemLoader(
-                searchpath=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+        self.template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+        self.template_env = Environment(loader=FileSystemLoader(searchpath=self.template_dir))
+
+    def add_building(self, urbanopt_building, mapper=None):
+        """
+        Add building to the translator.
+        :param urbanopt_building: an urbanopt_building
+        """
+
+        # TODO: Need to convert units, these should exist on the urbanopt_building object
+        # TODO: Abstract out the GeoJSON functionality
+        if mapper is None:
+            number_stories = urbanopt_building.feature.properties["number_of_stories"]
+            number_stories_above_ground = urbanopt_building.feature.properties["number_of_stories_above_ground"]
+            self.buildings.append(
+                {
+                    "area": urbanopt_building.feature.properties["floor_area"] * 0.092936,  # ft2 -> m2
+                    "building_id": urbanopt_building.feature.properties["id"],
+                    "building_type": urbanopt_building.feature.properties["building_type"],
+                    "floor_height": urbanopt_building.feature.properties["height"] * 0.3048,  # ft -> m
+                    "num_stories": urbanopt_building.feature.properties["number_of_stories_above_ground"],
+                    "num_stories_below_grade": number_stories - number_stories_above_ground,
+                    "year_built": urbanopt_building.feature.properties["year_built"],
+                }
             )
-        )
 
-    def to_modelica(self):
-        """ generate district model with arbitrary number of buildings connected to ETS"""
+    def to_modelica(self, scaffold):
+        """
+        Create TimeSeries models based on the data in the buildings and geojsons
 
+        :param scaffold: Scaffold object, Scaffold of the entire directory of the project.
+        """
+        curdir = os.getcwd()
+        rc_building_template = self.template_env.get_template("BuildingRCZ6.mot")
+        ets_template = self.template_env.get_template("CoolingIndirect.mot")
         district_template = self.template_env.get_template("District_InfiniteSource_nBuildings.mot")
 
-        # setup the data to be applied in template
-        district_data = {}
-        district_data['rc_buildings_count'] = self.nBuilding
+        building_names = []
+        try:
+            for building in self.buildings:
+                # create timeSeries building and save to the correct directory
+                print(f"Creating RCZ6 for building: {building['building_id']}")
 
-        # Here comes the Jina2 function: render()
-        templated_model = district_template.render(district_data=district_data)
-        print(templated_model)
+                # Path for building data
+                building_names.append(f"B{building['building_id']}")
+                b_modelica_path = ModelicaPath(
+                    f"B{building['building_id']}", scaffold.loads_path.files_dir, True
+                )
+                # print("\n $$$$$$ Yanfei building-id: ", building['building_id'], "$$$$$$\n")
 
-        # write templated ETS back to modelica file , to the tests folder for Dymola test
-        path_templated = os.path.join(self.directory_district_nBuildings,
-                                      "district_infinite_source_nRCBuildings_templated.mo")
-        if os.path.exists(path_templated):
-            os.remove(path_templated)
-        # os.makedirs( self.directory_district_nBuildings )
-        with open(path_templated, "w") as f:
-            f.write(templated_model)
+                # generate files with name:
+                # BuildingRCZ6.mo, CoolingIndirect.mo and CouplingETS_RCBuilding.mo
+                # They are created sequentially
+                # ----> 1.0 BuildingRCZ6
+                file_data = rc_building_template.render(
+                    project_name=scaffold.project_name,
+                    model_name=f"B{building['building_id']}"
+                )
+                with open(os.path.join(os.path.join(b_modelica_path.files_dir, "BuildingRCZ6.mo")), "w") as f:
+                    f.write(file_data)
 
-        return templated_model
+                # 2.0 ----> ETS,
+                # read parameters of ETS from parameters.json file
+                ets_model_type = self.system_parameters.get_param_by_building_id(
+                    building["building_id"], "ets_model"
+                )
+                ets_data = None
+                if ets_model_type == "Indirect Cooling":
+                    ets_data = self.system_parameters.get_param_by_building_id(
+                        building["building_id"],
+                        "ets_model_parameters.indirect_cooling"
+                    )
+                else:
+                    raise Exception("Only ETS Model of type 'Indirect Cooling' type enabled currently")
+
+                file_data = ets_template.render(
+                    project_name=scaffold.project_name,
+                    model_name=f"B{building['building_id']}",
+                    ets_data=ets_data,
+                )
+
+                with open(os.path.join(os.path.join(b_modelica_path.files_dir, "CoolingIndirect.mo")), "w") as f:
+                    f.write(file_data)
+
+                # 3.0 ----> CouplingETS_RCBuilding
+                district_data = {}
+                district_data['rc_buildings_count'] = self.nBuilding
+
+                district_new = district_template.render(
+                    project_name=scaffold.project_name,
+                    model_name=f"B{building['building_id']}",
+                    rc_name="BuildingRCZ6",
+                    ets_name="CoolingIndirect",
+                    district_data=district_data
+                )
+                with open(os.path.join(os.path.join(b_modelica_path.files_dir, "District_nBuildings_Infinite.mo")),
+                          "w") as f:
+                    f.write(district_new)
+
+        finally:
+            os.chdir(curdir)
+
+        # run post process to create the remaining project files for this building
+        self.post_process(scaffold, building_names)
+
+    def post_process(self, scaffold, building_names):
+        """
+        Cleanup the export of TimeSeries files into a format suitable for the district-based analysis. This includes
+        the following:
+
+            * Add a Loads project
+            * Add a project level project
+
+        :param scaffold: Scaffold object, Scaffold of the entire directory of the project.
+        :param building_names: list, names of the buildings that need to be cleaned up after export
+        :return: None
+        """
+        for b in building_names:
+            b_modelica_path = os.path.join(scaffold.loads_path.files_dir, b)
+            new_package = PackageParser.new_from_template(
+                b_modelica_path, b,
+                ["BuildingRCZ6", "CoolingIndirect", "District_nBuildings_Infinite"],
+                within=f"{scaffold.project_name}.Loads"
+            )
+            new_package.save()
+
+        # now create the Loads level package. This (for now) will create the package without considering any existing
+        # files in the Loads directory.
+        package = PackageParser.new_from_template(
+            scaffold.loads_path.files_dir, "Loads", building_names, within=f"{scaffold.project_name}"
+        )
+        package.save()
+
+        # now create the Package level package. This really needs to happen at the GeoJSON to modelica stage, but
+        # do it here for now to aid in testing.
+        pp = PackageParser.new_from_template(
+            scaffold.project_path, scaffold.project_name, ["Loads"]
+        )
+        pp.save()
