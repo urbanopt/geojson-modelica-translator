e48c619f1155e692f2f171631a4e487991fae363
Allen <aallen@nrel.gov>
2020-09-23 09:44:05 -0500

    Updates to spawn ets coupling.py

--- geojson_modelica_translator/model_connectors/spawn_ets_coupling.py
+++ geojson_modelica_translator/model_connectors/spawn_ets_coupling.py
@@ -146,17 +146,20 @@ class SpawnConnectorETS(model_connector_base):
                 data=template_data
             )
 
-            ets_model_type = self.system_parameters.get_param_by_building_id(
-                building["building_id"], "ets_model"
+            # ets_model_type = self.system_parameters.get_param_by_building_id(
+                # building["building_id"], "ets_model" ##AA revised this
+            #)
+			 ets_model_type = self.system_parameters.get_param_by_building_id(
+                 building["building_id"], "ets.ets_properties_cooling.ets_connection_type" ##AA added this 
             )
             ets_data = None
-            if ets_model_type == "Indirect Cooling":
+            if ets_model_type == "Indirect":
                 ets_data = self.system_parameters.get_param_by_building_id(
                     building["building_id"],
                     "ets_model_parameters.indirect_cooling"
                 )
             else:
-                raise Exception("Only ETS Model of type 'Indirect Cooling' type enabled currently")
+                raise Exception("Only ETS Model of type 'Indirect' type enabled currently") ##AA revised this 
 
             self.run_template(
                 cooling_indirect_template,
