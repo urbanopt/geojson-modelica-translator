ca714764b1728ef183bdd00540b20a7004d2b16a
Nathan Moore <nathan.moore@nrel.gov>
2020-11-30 13:18:35 -0700

    clean up docstring

--- geojson_modelica_translator/modelica/csv_modelica.py
+++ geojson_modelica_translator/modelica/csv_modelica.py
@@ -41,9 +41,9 @@ class CSVModelica(object):
         https://github.com/urbanopt/DES_HVAC/tree/develop/Measures/export_time_series_modelica, which is included
         in the URBANopt SDK (potentially via common-measures-gem).
         FIXME: this is in process as of 2020-11-18
-        TODO: Add support for input csv files directly from EnergyPlus (like it was before this PR). In that case,
-        this is specific to the Mass Flow Rate file only and requires the file format to contain the following headers
-        in this order:
+        TODO: Add support for input csv files directly from EnergyPlus (like it was before accepting input from the
+        above measure). In that case, this is specific to the Mass Flow Rate file only and requires the file format
+        to contain the following headers in this order:
 
         0: Date/Time, string, date time. This column isn't used and will be removed upon writing out.
         1: THWR, double, Temperature hot water return, degC
