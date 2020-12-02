85180b448e17075de2c381c2611cc5cd150f4317
Ted Summer <ted.summer2@gmail.com>
2020-12-01 17:02:10 -0700

    refactor: add uuid to heating indirect id

--- geojson_modelica_translator/model_connectors/energy_transfer_systems/heating_indirect.py
+++ geojson_modelica_translator/model_connectors/energy_transfer_systems/heating_indirect.py
@@ -34,6 +34,7 @@ from geojson_modelica_translator.model_connectors.energy_transfer_systems.energy
     EnergyTransferBase
 )
 from geojson_modelica_translator.modelica.input_parser import PackageParser
+from geojson_modelica_translator.utils import simple_uuid
 
 
 class HeatingIndirect(EnergyTransferBase):
@@ -41,7 +42,7 @@ class HeatingIndirect(EnergyTransferBase):
 
     def __init__(self, system_parameters):
         super().__init__(system_parameters)
-        self.id = 'MyHeatingIndirect'
+        self.id = 'heaInd_' + simple_uuid()
 
     def to_modelica(self, scaffold):
         """
