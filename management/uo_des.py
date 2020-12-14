6336d5baca1e811e2b9284e3e5f5eeefbe86960d
Nathan Moore <nathan.moore@nrel.gov>
2020-12-14 15:58:42 -0700

    make scenario_dir a required input

--- management/uo_des.py
+++ management/uo_des.py
@@ -12,7 +12,7 @@ def cli():
 @cli.command()
 @click.option('--sys_param_filename', default="test_sys_param.json",
               help='Name of systems parameters file to create. Include ".json" in your name')
-@click.option("--scenario_dir", help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory")
+@click.option("--scenario_dir", help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory", required=True)
 @click.option("--sys_param_template", default=Path.cwd() / "tests" / "geojson" / "data"
               / "time_series_template.json", help="Path to the template sys_param.json file")
 def sys_param(sys_param_filename, scenario_dir, sys_param_template):
