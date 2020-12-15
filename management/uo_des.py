79e69d30f20e4027916d14798d133fe033d84481
Nathan Moore <nathan.moore@nrel.gov>
2020-12-14 17:05:33 -0700

    make options prettier for developers to read

--- management/uo_des.py
+++ management/uo_des.py
@@ -10,11 +10,24 @@ def cli():
 
 
 @cli.command()
-@click.option('--sys_param_filename', default="test_sys_param.json",
-              help='Name of systems parameters file to create. Include ".json" in your name')
-@click.option("--scenario_dir", help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory", required=True)
-@click.option("--sys_param_template", default=Path.cwd() / "tests" / "geojson" / "data"
-              / "time_series_template.json", help="Path to the template sys_param.json file")
+@click.option(
+    '-f',
+    '--sys_param_filename',
+    default="test_sys_param.json",
+    help='Name of systems parameters file to create. Include ".json" in your name'
+)
+@click.option(
+    "-s",
+    "--scenario_dir",
+    help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory",
+    default=Path.cwd() / "tests" / "geojson" / "data" / "sdk_output_skeleton" / "run" / "baseline_15min"
+)
+@click.option(
+    "-t",
+    "--sys_param_template",
+    default=Path.cwd() / "tests" / "geojson" / "data" / "time_series_template.json",
+    help="Path to the template sys_param.json file"
+)
 def sys_param(sys_param_filename, scenario_dir, sys_param_template):
     """Test GMT CLI."""
     # click.echo(sys_param_filename)
