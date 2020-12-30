874e8aa6540a642f2dd1ab758caa6a5184f77305
Nathan Moore <nathan.moore@nrel.gov>
2020-12-30 16:29:52 -0700

    make cli work with new csv_to_sys_param location

--- management/uo_des.py
+++ management/uo_des.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 import click
-from geojson_modelica_translator.geojson.csv_to_sys_param import CSVToSysParam
+from geojson_modelica_translator.system_parameters.system_parameters import (
+    SystemParameters
+)
 
 
 @click.group()
@@ -13,7 +15,6 @@ def cli():
 @click.option(
     '-p',
     '--sys_param_filename',
-    default="test_sys_param.json",
     help='Name of systems parameters file to create. Include ".json" in your name',
     required=True
 )
@@ -21,28 +22,19 @@ def cli():
     "-s",
     "--scenario_dir",
     help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory",
-    default=Path.cwd() / "tests" / "geojson" / "data" / "sdk_output_skeleton" / "run" / "baseline_15min",
     required=True
 )
 @click.option(
     "-f",
     "--feature_file",
     help="Path to urbanopt sdk FeatureFile, probably in the root UO SDK project directory",
-    default=Path.cwd() / "tests" / "geojson" / "data" / "sdk_output_skeleton" / "example_project.json",
     required=True
 )
-@click.option(
-    "-t",
-    "--sys_param_template",
-    default=Path.cwd() / "tests" / "geojson" / "data" / "time_series_template.json",
-    help="Path to the template sys_param.json file",
-    required=True
-)
-def sys_param(sys_param_filename, scenario_dir, sys_param_template, feature_file):
+def sys_param(sys_param_filename, scenario_dir, feature_file):
     """GMT CLI."""
-    csv_to_sys_param = CSVToSysParam(
+    make_file = SystemParameters()
+    make_file.csv_to_sys_param(
+        sys_param_filename=Path(sys_param_filename),
         scenario_dir=Path(scenario_dir),
-        sys_param_template=Path(sys_param_template),
         feature_file=Path(feature_file)
     )
-    csv_to_sys_param.csv_to_sys_param(sys_param_filename)
