21f5401dcd0c019322677ad16b7f5f5dcec604b5
Nathan Moore <nathan.moore@nrel.gov>
2021-01-25 15:01:24 -0700

    use arguments instead of options

--- management/uo_des.py
+++ management/uo_des.py
@@ -12,43 +12,53 @@ def cli():
     pass
 
 
-@cli.command()
-@click.option(
-    '-p',
-    '--sys_param_filename',
-    help='Name of systems parameters file to create. Include ".json" in the name.',
-    required=True
+@cli.command(short_help="Create sys-param file")
+@click.argument(
+    'sys_param_filename',
+    type=click.Path(),
 )
-@click.option(
-    "-s",
-    "--scenario_dir",
-    help="Path to an urbanopt scenario, probably in the 'run' folder of your UO SDK project directory.",
-    required=True
+@click.argument(
+    "scenario_dir",
+    type=click.Path(exists=True),
 )
-@click.option(
-    "-f",
-    "--feature_file",
-    help="Path to urbanopt sdk FeatureFile, probably in the root UO SDK project directory.",
-    required=True
+@click.argument(
+    "feature_file",
+    type=click.Path(exists=True)
+)
+@click.argument(
+    "model-type",
+    default='time_series'
 )
 @click.option(
     '-o',
     '--overwrite',
     is_flag=True,
-    help="If a file already exists with the sys_param_filename, overwrite it.",
+    help="Delete and replace any existing file of the same name & location",
     default=False
 )
-@click.option(
-    "-m",
-    "--model",
-    help="Selet the model type you're using. Available options: time_series",
-    default='time_series',
-    required=True
-)
-def sys_param(model, sys_param_filename, scenario_dir, feature_file, overwrite):
-    """Create system parameters file using uo_sdk output"""
+def sys_param(model_type, sys_param_filename, scenario_dir, feature_file, overwrite):
+    """
+    Create system parameters file using uo_sdk output
+
+    SYS_PARAM_FILENAME: Path/name to sys-param file be created. Be sure to include the ".json" suffix.
+
+    SCENARIO_DIR: Path to sdk scenario folder with OpenStudio results.
+
+    FEATURE_FILE: Path to sdk json feature file with data about the buildings.
+
+    \b
+    MODEL_TYPE: selection for which kind of simulation this sys-param file will support.
+        Valid choices for MODEL_TYPE: "time_series"
+
+    \f
+    :param model_type: string, selection of which model type to use in the GMT
+    :param sys_param_filename: Path, location & name of output file to save
+    :param scenario_dir: Path, location of SDK scenario to create sys-param file from
+    :param feature_file: Path, location of SDK feature file
+    :param overwrite: Boolean, flag to overwrite an existing file of the same name/location
+    """
     SystemParameters.csv_to_sys_param(
-        model_type=model,
+        model_type=model_type,
         sys_param_filename=Path(sys_param_filename),
         scenario_dir=Path(scenario_dir),
         feature_file=Path(feature_file),
