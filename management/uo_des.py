3cabe80758651420ae691d049f898decd6894976
Nathan Moore <nathan.moore@nrel.gov>
2021-02-04 10:08:33 -0700

    ap & pc

--- management/uo_des.py
+++ management/uo_des.py
@@ -5,8 +5,8 @@ from geojson_modelica_translator.system_parameters.system_parameters import (
     SystemParameters
 )
 
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
