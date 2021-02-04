23111025c40d08b79f5b090c1495c3aa46b68bd0
Nicholas Long <nicholas.long@nrel.gov>
2021-02-04 11:50:08 -0700

    update copyrights to 2019-2021

--- geojson_modelica_translator/model_connectors/load_connectors/time_series_mft_ets_coupling.py
+++ geojson_modelica_translator/model_connectors/load_connectors/time_series_mft_ets_coupling.py
@@ -1,6 +1,6 @@
 """
 ****************************************************************************************************
-:copyright (c) 2019-2020 URBANopt, Alliance for Sustainable Energy, LLC, and other contributors.
+:copyright (c) 2019-2021 URBANopt, Alliance for Sustainable Energy, LLC, and other contributors.
 All rights reserved.
 Redistribution and use in source and binary forms, with or without modification, are permitted
 provided that the following conditions are met:
