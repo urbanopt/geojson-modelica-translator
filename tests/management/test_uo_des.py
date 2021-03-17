1c1c64e291323c9c77ba2c8b2981bdd403baf847
Nathan Moore <nathan.moore@nrel.gov>
2021-03-17 13:08:25 -0600

    don't run poetry inside poetry, that would be weird

--- tests/management/test_uo_des.py
+++ tests/management/test_uo_des.py
@@ -3,8 +3,10 @@ from shutil import rmtree
 from subprocess import run
 from unittest import TestCase
 
+# Integration tests that the CLI works as expected for an end user
 
-class CLITest(TestCase):
+
+class CLIIntegrationTest(TestCase):
     def setUp(self):
         self.data_dir = Path(__file__).parent / 'data'
         self.output_dir = Path(__file__).parent / 'output'
@@ -16,8 +18,8 @@ class CLITest(TestCase):
         if (self.output_dir / 'test_sys_param.json').exists():
             (self.output_dir / 'test_sys_param.json').unlink()
 
-        # run subprocess inside the poetry shell to ensure we're using the same venv
-        run(['poetry', 'run', 'uo_des', 'build-sys-param', self.output_dir / 'test_sys_param.json',
+        # run subprocess as if we're an end-user
+        run(['uo_des', 'build-sys-param', self.output_dir / 'test_sys_param.json',
              self.scenario_file_path, self.feature_file_path])
 
         # If this file exists, the cli command ran successfully
@@ -27,8 +29,8 @@ class CLITest(TestCase):
         if (self.output_dir / 'modelica_project' / 'Districts' / 'DistrictEnergySystem.mo').exists():
             rmtree(self.output_dir / 'modelica_project')
 
-        # run subprocess inside the poetry shell to ensure we're using the same venv
-        run(['poetry', 'run', 'uo_des', 'create-model', self.output_dir / 'test_sys_param.json',
+        # run subprocess as if we're an end-user
+        run(['uo_des', 'create-model', self.output_dir / 'test_sys_param.json',
              self.feature_file_path, self.output_dir / 'modelica_project'])
 
         # If this file exists, the cli command ran successfully
@@ -38,8 +40,8 @@ class CLITest(TestCase):
         if (self.output_dir / 'modelica_project_results').exists():
             rmtree(self.output_dir / 'modelica_project_results')
 
-        # run subprocess inside the poetry shell to ensure we're using the same venv
-        run(['poetry', 'run', 'uo_des', 'run-model', self.output_dir / 'modelica_project'])
+        # run subprocess as if we're an end-user
+        run(['uo_des', 'run-model', self.output_dir / 'modelica_project'])
 
         # If this file exists, the cli command ran successfully
         assert (self.output_dir / 'modelica_project_results' / 'modelica_project_Districts_DistrictEnergySystem_result.mat').exists()
