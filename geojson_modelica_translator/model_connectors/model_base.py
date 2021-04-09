f299c9d6d8abd0ab672f479e05f74970781773af
Nathan Moore <nathan.moore@nrel.gov>
2021-04-09 09:30:03 -0600

    use Path.as_posix to handle windows paths in model_base.py

--- geojson_modelica_translator/model_connectors/model_base.py
+++ geojson_modelica_translator/model_connectors/model_base.py
@@ -166,10 +166,10 @@ class ModelBase(object):
             raise Exception(f"Could not find mopt template '{self._template_instance}' in {self.template_dir}")
 
         # get template path relative to the package
-        template_filename = template.filename
+        template_filename = Path(template.filename).as_posix()
         _, template_filename = template_filename.rsplit('geojson_modelica_translator', 1)
 
-        return template.render(template_params), template_filename[1:]
+        return template.render(template_params), template_filename
 
     def to_dict(self, scaffold):
         return {
