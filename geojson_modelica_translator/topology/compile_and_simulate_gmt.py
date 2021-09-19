91822e2e23620268061b9ef75bd71740a3215980
Allen <aallen>
2021-09-18 22:44:02 -0600

    correcting mistake

--- geojson_modelica_translator/topology/compile_and_simulate_gmt.py
+++ geojson_modelica_translator/topology/compile_and_simulate_gmt.py
@@ -1,14 +1,11 @@
 # compile and simulate the model here, called separately by the measure
 # make sure this approach works with the measure ! it shouldn't need to pass any files=>should just be persisted in the directory
-
-import sys
+#  need to adjust path so that it can find modules
 
 from pyfmi import load_fmu
 from pymodelica import compile_fmu
 
-sys.path.append("/opt/openstudio/server/PyFMI/OCT_install/tmpinstalldir/install/Python")
-
-name = 'District.DistrictEnergySystem'
+name = 'Districts.DistrictEnergySystem'
 
 compile_options = {'runtime_log_to_file': True, 'generate_html_diagnostics': True, 'log_level': 6}
 
