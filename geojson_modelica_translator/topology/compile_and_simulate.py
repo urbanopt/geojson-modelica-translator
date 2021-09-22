aeff4046416016bd954e347883f29c2ddbbaa2b4
Allen <aallen>
2021-09-22 09:56:14 -0600

    updating compilation script

--- geojson_modelica_translator/topology/compile_and_simulate.py
+++ geojson_modelica_translator/topology/compile_and_simulate.py
@@ -1,21 +1,24 @@
-# compile and simulate the model here, called separately by the measure
-# make sure this approach works with the measure ! it shouldn't need to pass any files=>should just be persisted in the directory
-#  need to adjust path so that it can find modules
-
-# import pandas as pd
-
-# import buildingspy
-# import pkg_resources
-# from buildingspy.io.outputfile import Reader
 import os
 
+# Below may need to be uncommented
+# os.environ['JMODELICA_HOME'] = '/usr/local/JModelica'
+# os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
+# os.environ['MODELICAPATH'] = '/usr/local/JModelica/ThirdParty/MSL:/opt/openstudio/server/modelica-buildings: \
+# /opt/openstudio/server/modelica-buildings/Buildings: \
+# /opt/openstudio/server/geojson-modelica-translator/tests/management/data/sdk_project_scraps: \
+# /opt/openstudio/server/geojson-modelica-translator/tests/management/data/sdk_project_scraps/model_from_sdk'
+# os.environ['LDFLAGS'] = '/usr/local/JModelica/ThirdParty/Sundials/lib'
+# os.environ['LIBRARY_PATH'] = '/usr/local/JModelica/ThirdParty/Sundials/lib'
+# os.environ['LD_LIBRARY_PATH'] = '/usr/local/JModelica/ThirdParty/Sundials/lib'
+import pyfmi
 import pymodelica
-from pyfmi import load_fmu
-
-os.environ['JMODELICA_HOME'] = '/usr/local/JModelica'
 
 name = 'Districts.DistrictEnergySystem'
 file_path = '/opt/openstudio/server/geojson-modelica-translator/tests/management/data/sdk_project_scraps/'
+fmu_name = 'Districts_DistrictEnergySystem.fmu'
+
+starting_time = 0  # s
+end_time = 31536000  # s, full year
 
 
 def compile_and_simulate(bldg_1_conn):
@@ -23,6 +26,7 @@ def compile_and_simulate(bldg_1_conn):
 
     compile_options = {'runtime_log_to_file': True, 'generate_html_diagnostics': True, 'log_level': 6}
 
-    fmu = pymodelica.compile_fmu(name, compiler_options=compile_options, compiler_log_level='d', jvm_args='-Xmx10g', target='cs')
-    fmu_sim = load_fmu(fmu)
-    fmu_sim.simulate(start_time=0, final_time=5)  # could assign to res later # update simulation time!
+    # fmu=pymodelica.compile_fmu(name, compiler_options=compile_options, compiler_log_level='d', jvm_args='-Xmx10g', target='cs')
+    pymodelica.compile_fmu(name, compiler_options=compile_options, compiler_log_level='d', target='cs')
+    fmu_sim = pyfmi.load_fmu(fmu_name)
+    fmu_sim.simulate(start_time=starting_time, final_time=end_time)  # could assign to res later # update simulation time!
