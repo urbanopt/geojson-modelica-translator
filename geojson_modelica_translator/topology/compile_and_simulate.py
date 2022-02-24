3a33a487606a9f739c4b50f969865ce777c1674c
Allen <aallen@nrel.gov>
2022-02-24 14:14:32 -0700

    geojson_modelica_translator/topology/set_up_v2.sh

--- geojson_modelica_translator/topology/compile_and_simulate.py
+++ geojson_modelica_translator/topology/compile_and_simulate.py
@@ -17,4 +17,4 @@ def compile_and_simulate(bldg_1_conn):
     pymodelica.compile_fmu(name, compiler_options=compile_options, compiler_log_level='d', target='cs')
     # spawn modelica --create-fmu Districts.DistrictEnergySystem  # spawn approach to be implemented in future
     fmu_sim = load_fmu(fmu_name)
-    fmu_sim.simulate(start_time=0, final_time=86400)  # Simulation time can be updated
+    fmu_sim.simulate(start_time=0, final_time=30)  # Simulation time can be updated
