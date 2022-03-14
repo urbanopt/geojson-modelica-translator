f161409f17bb9ae72b67463d3cbf7b1e974d111b
Nathan Moore <nathan.moore@nrel.gov>
2022-03-14 17:11:49 -0600

    read sys-params, combine with pv template

--- geojson_modelica_translator/modelica/GMT_Lib/Electrical/AC/ThreePhasesBalanced/Sources/community_pv.py
+++ geojson_modelica_translator/modelica/GMT_Lib/Electrical/AC/ThreePhasesBalanced/Sources/community_pv.py
@@ -14,10 +14,10 @@ class CommunityPV(SimpleGMT):
         # There can be multiple community pv arrays so we need to loop over them
         for index, pvarray in enumerate(community_pv_params):
             pv_template_params = {
-                f'array_{index}_net_surface_area_m2': f'{pvarray}.net_surface_area',
-                f'array_{index}_nominal_voltage_V': f'{pvarray}.nominal_voltage',
-                f'array_{index}_surface_azimuth_deg': f'{pvarray}.surface_azimuth',
-                f'array_{index}_surface_tilt_deg': f'{pvarray}.surface_tilt',
+                'net_surface_area_m2': pvarray["net_surface_area"],
+                'nominal_voltage_V': pvarray["nominal_voltage"],
+                'surface_azimuth_deg': pvarray["surface_azimuth"],
+                'surface_tilt_deg': pvarray["surface_tilt"],
             }
-        # render template to final modelica file
-        self.to_modelica(output_dir=output_dir, model_name='PVPanels', param_data=pv_template_params)
+            # render template to final modelica file
+            self.to_modelica(output_dir=output_dir, model_name='PVPanels', param_data=pv_template_params, iteration=index)
