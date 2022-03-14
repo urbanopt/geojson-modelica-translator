5e8f697af3cc0a681b2f4778801d0516d92720fa
Nathan Moore <nathan.moore@nrel.gov>
2022-03-14 17:10:23 -0600

    hack to get multiple mofiles from a single template

--- geojson_modelica_translator/modelica/simple_gmt.py
+++ geojson_modelica_translator/modelica/simple_gmt.py
@@ -11,15 +11,21 @@ class SimpleGMT(ModelBase):
         """
         super().__init__(system_parameters, template_dir)
 
-    def to_modelica(self, output_dir: Path, model_name: str, param_data: dict) -> None:
+    def to_modelica(self, output_dir: Path, model_name: str, param_data: dict, iteration=None) -> None:
         """
         Render the template to a Modelica file.
         """
         # render template to final modelica file
         model_template = self.template_env.get_template(f"{model_name}.mot")
+
+        # FIXME: This is a hack to conditionally get the iteration number into the template.
+        # This should be done in a better way.
+        if iteration is None:
+            iteration = ''
+
         self.run_template(
             template=model_template,
-            save_file_name=output_dir / f"{model_name}.mo",
+            save_file_name=output_dir / f"{model_name}{iteration}.mo",
             project_name=output_dir.stem,
             data=param_data
         )
