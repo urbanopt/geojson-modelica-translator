f61b75c9f7b698250d7bde14d744935390adc83c
Nathan Moore <nathan.moore@nrel.gov>
2022-04-07 12:28:30 -0600

    do not validate geojson features supplying detailed models

--- geojson_modelica_translator/geojson/urbanopt_geojson.py
+++ geojson_modelica_translator/geojson/urbanopt_geojson.py
@@ -94,6 +94,8 @@ class UrbanOptGeoJson(object):
         for feature in self.data.features:
             if feature["properties"]["type"] == "Building":
                 building = UrbanOptLoad(feature)
+                if feature["properties"]["detailed_model_filename"]:  # Don't run validation on buildings with a detailed model
+                    continue
                 if not building_ids or building.id in building_ids:
                     errors = self.schemas.validate("building", building.feature.properties)
                     if errors:
