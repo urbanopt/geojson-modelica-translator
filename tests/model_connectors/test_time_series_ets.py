794cce7836013c34b924e384433ddb43ee16f8fd
Nathan Moore <nathan.moore@nrel.gov>
2022-04-19 08:16:02 -0600

    do not attempt to simulate mblv9 models with jmodelica, will not work

--- tests/model_connectors/test_time_series_ets.py
+++ tests/model_connectors/test_time_series_ets.py
@@ -104,7 +104,7 @@ class TimeSeriesETSTest(TestCaseBase):
         )
         district.to_modelica()
 
-        root_path = Path(district._scaffold.districts_path.files_dir)
-        self.run_and_assert_in_docker(root_path / 'DistrictEnergySystem.mo',
-                                      project_path=district._scaffold.project_path,
-                                      project_name=district._scaffold.project_name)
+        # root_path = Path(district._scaffold.districts_path.files_dir)
+        # self.run_and_assert_in_docker(root_path / 'DistrictEnergySystem.mo',
+        #                               project_path=district._scaffold.project_path,
+        #                               project_name=district._scaffold.project_name)
