3378587ca41e09f72dbae166f42036efcc8be1f4
Nicholas Long <1907354+nllong@users.noreply.github.com>
2022-05-24 15:58:07 -0600

    root_path formatting
    Co-authored-by: Nathan Moore <nathan.moore@nrel.gov>

--- tests/model_connectors/test_teaser.py
+++ tests/model_connectors/test_teaser.py
@@ -109,6 +109,6 @@ class TeaserModelConnectorSingleBuildingTest(TestCaseBase):
     @pytest.mark.simulation
     def test_simulate_teaser_single(self):
         root_path = Path(self.district._scaffold.districts_path.files_dir).resolve()
-        self.run_and_assert_in_docker(Path(root_path) / 'DistrictEnergySystem.mo',
+        self.run_and_assert_in_docker(root_path / 'DistrictEnergySystem.mo',
                                       project_path=self.district._scaffold.project_path,
                                       project_name=self.district._scaffold.project_name)
