d71280485aa974fb28fc3f0c6183d136c68e228a
Nathan Moore <nathan.moore@nrel.gov>
2022-06-09 16:01:41 -0600

    flesh out spawn.py

--- geojson_modelica_translator/modelica/lib/runner/spawn.py
+++ geojson_modelica_translator/modelica/lib/runner/spawn.py
@@ -1,34 +1,43 @@
 import argparse
 import os
 import re
+from pathlib import Path
 
 
-def compile_fmu(model_name, modelica_path):
+def compile_fmu(model_name, modelica_path, compiler):
     """
     Compile a modelica model.
-    This function shamelessly stolen from https://github.com/NREL/MegaBOP/blob/main/pymodelica/compiler.py#L11
+    This function shamelessly stolen from https://github.com/NREL/MegaBOP/blob/main/main/ pymodelica/compiler.py#L11
     """
     # Spawn's implementation doesn't currently handle *.mo files in the modelica_path,
     # it is expecting only directories,
     # however BOPTEST's parser will include wrapped.mo in the modelica_path.
     # This is a workaround to elliminate .mo items from modelica_path
     modelica_path = ' '.join([p for p in modelica_path if not re.match(r'.*\.mo$', p)])
+    mbl_path = f"{os.environ['MODELICAPATH']}/Buildings"
 
-    cmd = f'spawn modelica --modelica-path {modelica_path} --fmu-type ME --create-fmu {model_name}'
+    cmd = f'spawn modelica --create-fmu {model_name} --modelica-path {modelica_path} {mbl_path} --fmu-type ME --{compiler}'
     os.system(cmd)
     return f'{model_name}.fmu'
 
 
-def run(fmu_name, model_name, output_path, log_level):
+def run(fmu_name):
     """
-    Run a modelica model.
+    Run a modelica model with Spawn.
     """
+
+    # TODO: Decide if start, stop, or step should be exposed to the user.
+    cmd = f"spawn fmu --simulate {fmu_name} --start 0.0 --stop 86400 --step 300"
+    os.system(cmd)
+    return f'{fmu_name} has been simulated'
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', help='Action to perform on the model, complile, run, compile_and_run')
+    parser.add_argument('action', help='Action to perform on the model: complile, run, compile_and_run')
     parser.add_argument('model', help='Name of the model to run, if debug, then will use test PID model.')
+    parser.add_argument('modelica_path', help='Path to the project folder.')
+    parser.add_argument('compiler', help='Compiler to use.', default='optimica')
     # Since this command is passed with jm_ipython, you can't use the -- args (e.g., --compile).
     # So we are just passing in the action and then the model to act on.
     # The actions can be (e.g., complile, run, compile_and_run)
@@ -43,15 +52,15 @@ if __name__ == "__main__":
         if args.model == 'debug':
             model = "Buildings.Controls.OBC.CDL.Continuous.Validation.LimPID"
         else:
-            if os.path.isfile(args.model):
+            if Path(args.model).is_file():
                 model = args.model.replace(os.path.sep, '.')[:-3]
-        fmu_name = compile(model)
+        fmu_name = compile(model, args.modelica_path, args.compiler)
 
     if args.action == 'run' or args.action == 'compile_and_run':
         # Run the FMU either that is passed in or from the previous step
         if not fmu_name:
             fmu_name = args.model
-        if os.path.exists(fmu_name):
+        if Path(fmu_name).exists():
             run(fmu_name)
         else:
             print("FMU model does not exist: {}".format(fmu_name))
