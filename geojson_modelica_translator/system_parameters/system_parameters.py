d6f8e94bdab57130745ed3fa8b2b004ae68a29a2
Nathan Moore <nathan.moore@nrel.gov>
2022-06-29 20:20:16 -0600

    make the create-sys-param command slightly less brittle

--- geojson_modelica_translator/system_parameters/system_parameters.py
+++ geojson_modelica_translator/system_parameters/system_parameters.py
@@ -822,9 +822,10 @@ class SystemParameters(object):
                     building['load_model_parameters']['time_series']['filepath'] = str(measure_file_path.resolve())
                 if (measure_file_path.suffix == '.csv') and ('_export_time_series_modelica' in str(measure_folder_name)):
                     mfrt_df = pd.read_csv(measure_file_path)
-                    building_nominal_mfrt = mfrt_df['massFlowRateHeating'].max().round(3)
-                    building['ets_model_parameters']['indirect']['nominal_mass_flow_building'] = float(
-                        building_nominal_mfrt)
+                    if 'massFlowRateHeating' in mfrt_df.columns:
+                        building_nominal_mfrt = mfrt_df['massFlowRateHeating'].max().round(3)
+                        building['ets_model_parameters']['indirect']['nominal_mass_flow_building'] = float(
+                            building_nominal_mfrt)
                 district_nominal_mfrt += building_nominal_mfrt
 
         # Remove template buildings that weren't used or don't have successful simulations with modelica outputs
