386940c493113a7f8d1933ccf73e569d1ee49b6a
Nathan Moore <nathan.moore@nrel.gov>
2022-07-26 13:21:41 -0600

    crazy way of including paths in the container, but it works

--- geojson_modelica_translator/modelica/lib/runner/spawn.py
+++ geojson_modelica_translator/modelica/lib/runner/spawn.py
@@ -30,9 +30,11 @@ def compile_fmu(model_name, modelica_path, compiler):
     logger.info(f"Modelica path is {modelica_path}")
     # The MBL path can be passed in from docker (spawn_docker.sh) which joins paths
     # with a colon. Need to split this back out to space separated paths.
-    mbl_path = f"{os.environ['MODELICAPATH']}/Buildings".split(':')[-1]
+    # FIXME: I don't know how env vars work in Docker. The container version of both the modelica_path and the mbl_path
+    # are inside the mbl_path variable. We need both, but somehow they're both in the mbl_path variable.
+    mbl_path = ' '.join([p for p in f"{os.environ['MODELICAPATH']}/Buildings".split(':')])
     logger.info(f"MBL path is {mbl_path}")
-    cmd = f"spawn modelica --create-fmu {model_name} --modelica-path {modelica_path} {mbl_path} --fmu-type ME --{compiler}"
+    cmd = f"spawn modelica --create-fmu {model_name} --modelica-path {mbl_path} --fmu-type ME --{compiler}"
     logger.info(f"Calling spawn compile with '{cmd}'")
     # Uncomment this section and rebuild the container in order to pause the container
     # to inpsect the container and test commands.
