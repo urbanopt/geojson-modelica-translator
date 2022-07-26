b43d403d2963055575d63845ce255e4fa9997144
Nathan Moore <nathan.moore@nrel.gov>
2022-07-26 13:07:48 -0600

    wip: clean up paths transferred to spawn.py

--- geojson_modelica_translator/modelica/lib/runner/spawn.py
+++ geojson_modelica_translator/modelica/lib/runner/spawn.py
@@ -3,7 +3,6 @@
 import argparse
 import logging
 import os
-import re
 from pathlib import Path
 from typing import Optional
 
@@ -26,15 +25,12 @@ def compile_fmu(model_name, modelica_path, compiler):
     and only lightly modified.
     """
 
-    # Spawn's implementation doesn't currently handle *.mo files in the modelica_path.
+    # FYI Spawn's implementation doesn't currently handle *.mo files in the modelica_path.
     # It is expecting only directories.
-    # This is a workaround to eliminate .mo items from modelica_path
-    # FIXME: GMT is passing a dir, correct? So this modelica_path line isn't necessary?
-    modelica_path = ' '.join([p for p in modelica_path if not re.match(r'.*\.mo$', p)])
     logger.info(f"Modelica path is {modelica_path}")
     # The MBL path can be passed in from docker (spawn_docker.sh) which joins paths
     # with a colon. Need to split this back out to space separated paths.
-    mbl_path = ' '.join([p for p in f"{os.environ['MODELICAPATH']}/Buildings".split(':')])
+    mbl_path = f"{os.environ['MODELICAPATH']}/Buildings".split(':')[-1]
     logger.info(f"MBL path is {mbl_path}")
     cmd = f"spawn modelica --create-fmu {model_name} --modelica-path {modelica_path} {mbl_path} --fmu-type ME --{compiler}"
     logger.info(f"Calling spawn compile with '{cmd}'")
