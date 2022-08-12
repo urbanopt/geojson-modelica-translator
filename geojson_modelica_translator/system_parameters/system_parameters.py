e696eeacdd89e8db9581491d735dd221098a8fdc
Nathan Moore <nathan.moore@nrel.gov>
2022-08-12 08:07:01 -0600

    update sys-params.py to work with explicitly labeled 4gdhc systems

--- geojson_modelica_translator/system_parameters/system_parameters.py
+++ geojson_modelica_translator/system_parameters/system_parameters.py
@@ -72,7 +72,7 @@ class SystemParameters(object):
         {"json_path": "$.buildings.*[?load_model=rc].load_model_parameters.rc.mos_weather_filename"},
         {"json_path": "$.buildings.*[?load_model=time_series].load_model_parameters.time_series.filepath"},
         {"json_path": "$.buildings.*[?load_model=time_series_massflow_temperature].load_model_parameters.time_series.filepath"},
-        {"json_path": "$.district_system.default.central_cooling_plant_parameters.weather_filepath"},
+        {"json_path": "$.district_system.4GDHC.central_cooling_plant_parameters.weather_filepath"},
         {"json_path": "$.combined_heat_and_power_systems.*.performance_data_path"}
     ]
 
@@ -840,7 +840,7 @@ class SystemParameters(object):
 
         # Update district sys-param settings
         # Parens are to allow the line break
-        (self.param_template['district_system']['default']
+        (self.param_template['district_system']['4GDHC']
             ['central_cooling_plant_parameters']['weather_filepath']) = str(weather_path)
 
         if microgrid:
