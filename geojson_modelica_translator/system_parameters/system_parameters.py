576ec6fa2f05aa7acfdf9c7fae3a235d663bbec2
Nathan Moore <nathan.moore@nrel.gov>
2022-08-17 14:37:04 -0600

    fix xpath path to parse lists correctly

--- geojson_modelica_translator/system_parameters/system_parameters.py
+++ geojson_modelica_translator/system_parameters/system_parameters.py
@@ -68,7 +68,7 @@ class SystemParameters(object):
         {"json_path": "$.buildings.*[?load_model=time_series].load_model_parameters.time_series.filepath"},
         {"json_path": "$.buildings.*[?load_model=time_series_massflow_temperature].load_model_parameters.time_series.filepath"},
         {"json_path": "$.district_system.fourthGDHC.central_cooling_plant_parameters.weather_filepath"},
-        {"json_path": "$.combined_heat_and_power_systems.*.performance_data_path"}
+        {"json_path": "$.combined_heat_and_power_systems.[*].performance_data_path"}
     ]
 
     def __init__(self, filename=None):
