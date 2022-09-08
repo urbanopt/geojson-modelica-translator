fec9971e1d323c293d857d3efd4427d5f8a0a2e2
Nathan Moore <nathan.moore@nrel.gov>
2022-09-08 12:31:15 -0600

    make test_time_series pass, only works for 4G models currently

--- tests/model_connectors/test_time_series.py
+++ tests/model_connectors/test_time_series.py
@@ -89,7 +89,8 @@ class TimeSeriesModelConnectorSingleBuildingTest(TestCaseBase):
 
         self.root_path = os.path.abspath(os.path.join(self.scaffold.loads_path.files_dir, 'B5a6b99ec37f4de7f94020090'))
         files = [
-            os.path.join(self.root_path, 'building.mo'),
+            os.path.join(self.root_path, 'BuildingTimeSeries.mo'),
+            # TODO: adapt this for 5G systems (looking for building.mo instead of BuildingTimeSeries.mo), or have a whole new test file
         ]
 
         # verify that there are only 2 files that matter (coupling and building)
@@ -118,13 +119,14 @@ class TimeSeriesModelConnectorSingleBuildingTest(TestCaseBase):
 
         self.root_path = os.path.abspath(os.path.join(self.scaffold.loads_path.files_dir, 'B5a6b99ec37f4de7f94020090'))
         files = [
-            os.path.join(self.root_path, 'building.mo'),
+            os.path.join(self.root_path, 'BuildingTimeSeries.mo'),
+            # TODO: adapt this for 5G systems (looking for building.mo instead of BuildingTimeSeries.mo), or have a whole new test file
         ]
 
         # verify that there are only 2 files that matter (coupling and building)
         for file in files:
             self.assertTrue(os.path.exists(file), f"File does not exist: {file}")
 
-        self.run_and_assert_in_docker(os.path.join(self.root_path, 'building.mo'),
+        self.run_and_assert_in_docker(os.path.join(self.root_path, 'BuildingTimeSeries.mo'),
                                       project_path=self.scaffold.project_path,
                                       project_name=self.scaffold.project_name)
