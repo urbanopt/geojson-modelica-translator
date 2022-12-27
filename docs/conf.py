bd0c0c4373130989296088438aee50cde947097d
Nathan Moore <nathan.moore@nrel.gov>
2022-12-27 11:23:52 -0700

    add sphinx extension to make section links work easily

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
     'sphinx-jsonschema',
 ]
 
