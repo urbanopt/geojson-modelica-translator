c6197120ea842c92ca6be0d919c715f7fcfff06c
Nathan Moore <nathan.moore@nrel.gov>
2023-04-28 09:25:05 -0600

    refactor to reduce line-length

--- geojson_modelica_translator/modelica/GMT_Lib/Electrical/AC/ThreePhasesBalanced/Loads/capacitor.py
+++ geojson_modelica_translator/modelica/GMT_Lib/Electrical/AC/ThreePhasesBalanced/Loads/capacitor.py
@@ -10,12 +10,12 @@ class Capacitor(SimpleGMTBase):
         super().__init__(self.system_parameters, self.template_dir)
 
     def build_from_template(self, output_dir: Path):
-        capacitor_params = self.system_parameters.get_param("$.capacitor_banks")
+        cap_params = self.system_parameters.get_param("$.capacitor_banks")
         # There can be multiple capacitors so we need to loop over them
-        for index, capacitor in enumerate(capacitor_params):
-            capacitor_params = {
+        for index, capacitor in enumerate(cap_params):
+            cap_params = {
                 'nominal_capacity': capacitor["nominal_capacity"],
                 'model_name': f"Capacitor{index}",
             }
             # render template to final modelica file
-            self.to_modelica(output_dir=output_dir, model_name='Capacitor', param_data=capacitor_params, iteration=index)
+            self.to_modelica(output_dir=output_dir, model_name='Capacitor', param_data=cap_params, iteration=index)
