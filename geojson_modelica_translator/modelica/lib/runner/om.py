8f63baf20b83f87542256c265e4687e1d6bf6e34
Nathan Moore <nathan.moore@nrel.gov>
2023-05-03 09:10:17 -0600

    wip: OM Docker code partial setup. Needs at least more Dockerfile work

---
+++ geojson_modelica_translator/modelica/lib/runner/om.py
@@ -0,0 +1,90 @@
+# This file is used as a Python-based CLI into OpenModelica.
+
+import argparse
+import logging
+import os
+from pathlib import Path
+from typing import Optional
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s: %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+)
+
+
+# FIXME: what about model_path? See the confusion in spawn.py
+def compile_fmu(model_name):
+    """Compile a modelica model with OpenModelica.
+
+    (Do not rename this function to `compile` as it is reserved in Python.)
+
+    Usage: omc [Options] (Model.mo | Script.mos) [Libraries | .mo-files]
+             * Libraries: Fully qualified names of libraries to load before processing Model or Script.
+             The libraries should be separated by spaces: Lib1 Lib2 ... LibN.
+    """
+
+    # The MBL path can be passed in from docker (om_docker.sh) which joins paths
+    # with a colon. Need to split this back out to space separated paths.
+    mbl_path = ' '.join([p for p in f"{os.environ['MODELICAPATH']}/Buildings".split(':')])
+    logger.info(f"MBL path is {mbl_path}")
+    cmd = f"omc {model_name} Modelica {mbl_path} "
+    logger.info(f"Calling OpenModelica compile with '{cmd}'")
+    # Uncomment this section and rebuild the container in order to pause the container
+    # to inpsect the container and test commands.
+    # import time
+    # time.sleep(10000)
+    os.system(cmd)
+    return f"{model_name}.fmu"
+
+
+def run(fmu_name, start: Optional[int], stop: Optional[int], step: Optional[int]):
+    """Run a modelica model with OpenModelica.
+    Usage: OMSimulator [Options] [Lua script] [FMU] [SSP file]
+    """
+
+    cmd = f"OMSimulator {start} {stop} {step} {fmu_name}"
+    logger.info(f"Calling OpenModelica simulator with '{cmd}'")
+    os.system(cmd)
+    return f'{fmu_name} has been simulated'
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', help='Action to perform on the model: compile, run, compile_and_run')
+    parser.add_argument('model', help='Name of the model to run, if debug, then will use test PID model. Can be an mo file or an FMU')  # noqa
+    parser.add_argument('modelica_path', help='Path to the project folder.')
+    parser.add_argument('start_time', help='Start time of the simulation.', nargs='?')
+    parser.add_argument('end_time', help='End time of the simulation.', nargs='?')
+    parser.add_argument('sim_step', help='Time step of the simulation.', nargs='?')
+
+    # Since this command is passed with om.py, you can't use the -- args (e.g., --compile).
+    # So we are just passing in the action and then the model to act on.
+    # The actions can be (e.g., complile, run, compile_and_run)
+    args = parser.parse_args()
+
+    logger.info(f'args: {args}')
+
+    if args.action == 'help':
+        print(parser.print_help())
+
+    fmu_name = None
+    if args.action == 'compile' or args.action == 'compile_and_run':
+        model = args.model
+        if args.model == 'debug':
+            model = "Buildings.Controls.OBC.CDL.Continuous.Validation.LimPID"
+        else:
+            if Path(args.model).is_file():
+                model = args.model.replace(os.path.sep, '.')[:-3]
+        logger.info(f'Compiling {model}')
+        fmu_name = compile_fmu(model, args.modelica_path)
+
+    if args.action == 'run' or args.action == 'compile_and_run':
+        # Run the FMU either that is passed in or from the previous step
+        if not fmu_name:
+            fmu_name = args.model
+        if Path(fmu_name).exists():
+            run(fmu_name, args.start_time, args.end_time, args.sim_step)
+        else:
+            print(f"FMU model does not exist: {fmu_name}")
---
+++ geojson_modelica_translator/modelica/lib/runner/om_docker.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# :copyright (c) URBANopt, Alliance for Sustainable Energy, LLC, and other contributors.
+# See also https://github.com/urbanopt/geojson-modelica-translator/blob/develop/LICENSE.md
+
+DOCKER_USERNAME=openmodelica/openmodelica
+IMG_NAME=v1.21.0-minimal
+
+# Catch signals to kill the container if it is interrupted
+# https://www.shellscript.sh/trap.html
+# Addresses https://github.com/urbanopt/geojson-modelica-translator/issues/384
+trap cleanup 1 2 3 6
+
+cleanup()
+{
+  echo "Caught Signal ... cleaning up."
+  rm -rf /tmp/temp_*.$$
+  echo "Done cleanup ... quitting."
+  exit 1
+}
+
+# Function declarations
+function create_mount_command()
+# Split path somehow. Replace double-slashes with single-slashes, to ensure compatibility with
+# Windows paths.
+{
+   local pat="$1"
+   # Each entry in pat will be a mounted read-only volume
+   local mnt_cmd=""
+   for ele in ${pat//:/ }; do
+      mnt_cmd="${mnt_cmd} -v ${ele}:/mnt${ele}:ro"
+   done
+
+   # On Darwin, the exported temporary folder needs to be /private/var/folders, not /var/folders
+   # see https://askubuntu.com/questions/600018/how-to-display-the-paths-in-path-separately
+   if [ `uname` == "Darwin" ]; then
+       mnt_cmd=`echo ${mnt_cmd} | sed -e 's| /var/folders/| /private/var/folders/|g'`
+   fi
+   echo "${mnt_cmd}"
+}
+
+function update_path_variable()
+{
+  # Prepend /mnt/ in front of each entry of a PATH variable in which the arguments are
+  # separated by a colon ":"
+  # This allows for example to create the new MODELICAPATH
+  local pat="$1"
+  local new_pat=`(set -f; IFS=:; printf "/mnt%s:" ${pat})`
+  # Cut the trailing ':'
+  new_pat=${new_pat%?}
+  echo "${new_pat}"
+}
+
+# Export the MODELICAPATH
+if [ -z ${MODELICAPATH+x} ]; then
+    MODELICAPATH=`pwd`
+else
+    # Add the current directory to the front of the Modelica path.
+    # This will export the directory to the docker, and also set
+    # it in the MODELICAPATH so that JModelica finds it.
+    MODELICAPATH=`pwd`:${MODELICAPATH}
+fi
+
+# Create the command to mount all directories in read-only mode
+# a) for MODELICAPATH
+MOD_MOUNT=`create_mount_command ${MODELICAPATH}`
+# b) for PYTHONPATH
+PYT_MOUNT=`create_mount_command ${PYTHONPATH}`
+
+# Prepend /mnt/ in front of each entry, which will then be used as the MODELICAPATH
+DOCKER_MODELICAPATH=`update_path_variable ${MODELICAPATH}`
+DOCKER_PYTHONPATH=`update_path_variable ${PYTHONPATH}`
+
+# If the current directory is part of the argument list,
+# replace it with . as the container may have a different file structure
+cur_dir=`pwd`
+bas_nam=`basename ${cur_dir}`
+
+# Set variable for shared directory
+sha_dir=`dirname ${cur_dir}`
+
+docker run \
+  ${MOD_MOUNT} \
+  ${PYT_MOUNT} \
+  -e DISPLAY=${DISPLAY} \
+  -e MODELICAPATH=${DOCKER_MODELICAPATH} \
+  -e PYTHONPATH=${DOCKER_PYTHONPATH} \
+  -v ${sha_dir}:/mnt/shared \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --rm \
+  ${DOCKER_USERNAME}/${IMG_NAME} /bin/bash -c \
+  "cd /mnt/shared/${bas_nam} && \
+  python /mnt/lib/om.py '$1' '$2' '$3' '$4'"
+exit $?
