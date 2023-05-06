8c396f4b0dc76a38d9dc97c0d99d738ce659fe29
Nicholas Long <nicholas.long@nrel.gov>
2023-05-06 07:15:15 -0600

    reduce delta t to 5C

--- geojson_modelica_translator/modelica/GMT_Lib/DHC/DHC_5G_waste_heat_GHX.py
+++ geojson_modelica_translator/modelica/GMT_Lib/DHC/DHC_5G_waste_heat_GHX.py
@@ -81,8 +81,8 @@ class DHC5GWasteHeatAndGHX(SimpleGMTBase):
             template_data['building_load_files'].append(f"modelica://{rel_path_name}")  # type: ignore
 
         # 5: Calculate the mass flow rates (kg/s) for the heating and cooling networks peak load (in Watts)
-        #    (assuming 10C delta T and 4.18 Cp (kJ/kgK)). Add 1.5x the peak for oversizing
-        delta_t = 10
+        #    (assuming 5C delta T [since 5G] and 4.18 Cp (kJ/kgK)). Add 1.5x the peak for oversizing
+        delta_t = 5
         heating_flow_rate = 1.5 * total_heating_load / (1000 * delta_t * 4.18)
         cooling_flow_rate = 1.5 * total_cooling_load / (1000 * delta_t * 4.18)
         swh_flow_rate = 1.5 * total_swh_load / (1000 * delta_t * 4.18)
