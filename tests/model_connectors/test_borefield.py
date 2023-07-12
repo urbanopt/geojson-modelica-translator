cf06e6d9b5cb7443f3eb1b7d425e93483913914d
Nathan Moore <nathan.moore@nrel.gov>
2023-07-12 11:47:52 -0600

    update borefield pytest skip reason with more detail

--- tests/model_connectors/test_borefield.py
+++ tests/model_connectors/test_borefield.py
@@ -63,7 +63,7 @@ class DistrictSystemTest(TestCaseBase):
         assert (root_path / 'DistrictEnergySystem.mo').exists()
 
     @pytest.mark.simulation
-    @pytest.mark.skip(reason="OMC failure: Error: Variable None not found in scope Borefield.")
+    @pytest.mark.skip(reason="Need to set use_m_flow_in=false. Then simulation fails in Linux, works on Windows.")
     def test_simulate_district_system(self):
         self.run_and_assert_in_docker(
             f'{self.district._scaffold.project_name}.Districts.DistrictEnergySystem',
