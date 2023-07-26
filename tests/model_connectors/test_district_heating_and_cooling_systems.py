03eac007c5326095effd48047b8be4257ea3eb40
Nathan Moore <nathan.moore@nrel.gov>
2023-07-26 14:26:53 -0600

    add start/stop/step times to dhc test setup

--- tests/model_connectors/test_district_heating_and_cooling_systems.py
+++ tests/model_connectors/test_district_heating_and_cooling_systems.py
@@ -107,12 +107,15 @@ class DistrictHeatingAndCoolingSystemsTest(TestCaseBase):
     #                                       project_name=self.district._scaffold.project_name)
 
     @pytest.mark.simulation
-    @pytest.mark.skip("OMC Failed. Simulation completed, but lots of errors in stdout.log")
+    # @pytest.mark.skip("OMC Failed. Simulation completed, but lots of errors in stdout.log")
     def test_simulate_district_heating_and_cooling_systems(self):
         self.run_and_assert_in_docker(
             f'{self.district._scaffold.project_name}.Districts.DistrictEnergySystem',
             file_to_load=self.district._scaffold.package_path,
-            run_path=self.district._scaffold.project_path
+            run_path=self.district._scaffold.project_path,
+            start_time=17280000,  # Day 200 (in seconds) (Run in summer to keep chiller happy)
+            stop_time=17366400,  # For 1 day duration (in seconds)
+            step_size=90  # (in seconds)
         )
 
         #
