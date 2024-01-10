ddeb985f1adce1ff5469cf2ccbe4b3a2badfd1ee
Nathan Moore <nathan.moore@nrel.gov>
2024-01-10 13:20:29 -0700

    re-enable gmt_lib tests now that we are using ubuntu 20

--- tests/GMT_Lib/test_gmt_lib_des.py
+++ tests/GMT_Lib/test_gmt_lib_des.py
@@ -35,7 +35,6 @@ env = Environment(
 class GmtLibDesTest(unittest.TestCase):
 
     @pytest.mark.simulation
-    @pytest.mark.skip('Invalid root in initialization. Skipping until LBNL helps')
     def test_5G_des_waste_heat_and_ghx(self):
         # -- Setup
         package_output_dir = PARENT_DIR / 'output'
@@ -102,7 +101,6 @@ class GmtLibDesTest(unittest.TestCase):
         assert success is True
 
     @pytest.mark.simulation
-    @pytest.mark.skip('Invalid root in initialization. Skipping until LBNL helps')
     def test_5G_des_waste_heat_and_ghx_variable(self):
         # -- Setup
         package_output_dir = PARENT_DIR / 'output'
