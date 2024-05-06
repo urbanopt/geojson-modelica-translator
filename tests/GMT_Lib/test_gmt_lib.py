475cae340d100b3dfcf27d523ac221f49a608d86
Nathan Moore <nathan.moore@nrel.gov>
2024-05-06 09:37:23 -0600

    skip separately building a steam model during testing

--- tests/GMT_Lib/test_gmt_lib.py
+++ tests/GMT_Lib/test_gmt_lib.py
@@ -604,7 +604,7 @@ def test_simulate_transformer():
     assert success is True
 
 
-# @pytest.mark.skip(reason="This functionality is entirely captured by asdf")
+@pytest.mark.skip(reason="This functionality is entirely captured by test_simulate_steam_example")
 def test_build_steam_example():
     # -- Setup
     package_output_dir = PARENT_DIR / "output" / "SteamExample"
