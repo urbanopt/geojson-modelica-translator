72842678e27d00b3d8b9cdbc4fa716c902592cf7
Jing Wang <jwang5@nrel.gov>
2024-06-04 14:20:34 -0600

    Modify test to instantiate ground coupling

--- tests/model_connectors/test_district_single_ghe.py
+++ tests/model_connectors/test_district_single_ghe.py
@@ -11,6 +11,7 @@ from geojson_modelica_translator.model_connectors.couplings.graph import Couplin
 from geojson_modelica_translator.model_connectors.districts.district import District
 from geojson_modelica_translator.model_connectors.load_connectors.time_series import TimeSeries
 from geojson_modelica_translator.model_connectors.networks.network_distribution_pump import NetworkDistributionPump
+from geojson_modelica_translator.model_connectors.networks.ground_coupling import GroundCoupling
 from geojson_modelica_translator.model_connectors.plants.borefield import Borefield
 from geojson_modelica_translator.system_parameters.system_parameters import SystemParameters
 from tests.base_test_case import TestCaseBase
@@ -28,7 +29,7 @@ class DistrictSystemTest(TestCaseBase):
         self.gj = UrbanOptGeoJson(filename)
 
         # load system parameter data
-        filename = Path(self.data_dir) / "system_params_ghe_2.json"
+        filename = Path(self.data_dir) / "system_params_ghe_3.json"
         sys_params = SystemParameters(filename)
 
         # create borefield
@@ -36,6 +37,9 @@ class DistrictSystemTest(TestCaseBase):
 
         # create ambient water stub
         ambient_water_stub = NetworkDistributionPump(sys_params)
+
+        # create ground coupling
+        ground_coupling = GroundCoupling(sys_params)
 
         # create the couplings and graph
         all_couplings = []
