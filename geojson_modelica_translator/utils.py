5040214e41568f9d8725d32e800e2a7e54882201
Nathan Moore <nathan.moore@nrel.gov>
2024-08-27 11:51:18 -0600

    enforce Path type to load loop_order

--- geojson_modelica_translator/utils.py
+++ geojson_modelica_translator/utils.py
@@ -48,7 +48,7 @@ def load_loop_order(system_parameters_file: Path) -> list:
     :param system_parameters_file: Path to the system parameters file
     :return: list of building & ghe ids in loop order
     """
-    loop_order_path = system_parameters_file.parent / "_loop_order.json"
+    loop_order_path = Path(system_parameters_file).parent / "_loop_order.json"
     if not loop_order_path.is_file():
         raise FileNotFoundError(f"Loop order file not found at {loop_order_path}")
     return json.loads(loop_order_path.read_text())
