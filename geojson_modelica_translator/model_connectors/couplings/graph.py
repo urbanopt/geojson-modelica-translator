5fc399c0752d979ca0f818cf18663bee991c84c7
Nathan Moore <nathan.moore@nrel.gov>
2024-08-30 09:42:33 -0600

    linting

--- geojson_modelica_translator/model_connectors/couplings/graph.py
+++ geojson_modelica_translator/model_connectors/couplings/graph.py
@@ -2,6 +2,7 @@
 # See also https://github.com/urbanopt/geojson-modelica-translator/blob/develop/LICENSE.md
 
 from collections import defaultdict
+
 from geojson_modelica_translator.model_connectors.plants.plant_base import PlantBase
 
 
@@ -134,25 +135,24 @@ class CouplingGraph:
             if coupling.id == coupling_id:
                 return coupling
         raise Exception(f'No coupling found with id "{coupling_id}"')
-    
-        
+
     def get_ghe_id(self, coupling_id):
         """If there's a GHE model in the coupling, it returns the ghe_id of the model. Else
         it returns None.
 
         :return: ghe_id | None
         """
-        
+
         coupling = self.get_coupling(coupling_id)
         model_a, model_b = coupling._model_a, coupling._model_b
-        
+
         if coupling._get_model_superclass(model_a) is PlantBase and "Borefield" in model_a.model_name:
             return model_a.ghe_id
         elif coupling._get_model_superclass(model_b) is PlantBase and "Borefield" in model_b.model_name:
             return model_b.ghe_id
 
         return None
-    
+
     def get_other_model(self, coupling_id, model_id):
         """Returns the other model in the coupling
 
@@ -165,5 +165,5 @@ class CouplingGraph:
         elif model_id == coupling._model_b.id:
             return coupling._model_a
         raise Exception(
-            f'Provided model, "{model_id}", is not part of the coupling ({coupling._model_a.id}, {coupling._model_b.id})'
+            f'Provided model, "{model_id}", is not part of coupling: ({coupling._model_a.id}, {coupling._model_b.id})'
         )
