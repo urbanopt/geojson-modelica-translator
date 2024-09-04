a9e9e019823153af175e44856c6ee5ca745b7d1d
Nathan Moore <nathan.moore@nrel.gov>
2024-09-04 08:34:09 -0600

    enable all EPW weather locations

--- geojson_modelica_translator/system_parameters/system_parameters.py
+++ geojson_modelica_translator/system_parameters/system_parameters.py
@@ -207,12 +207,111 @@ class SystemParameters:
                 "Malformed location, needs underscores of location "
                 "(e.g., USA_NY_Buffalo-Greater.Buffalo.Intl.AP.725280_TMY3.mos)"
             )
-
         # download file from energyplus website
-        weatherfile_url = (
-            "https://energyplus-weather.s3.amazonaws.com/north_and_central_america_wmo_region_4/"
-            f"{weatherfile_country}/{weatherfile_state}/{p_download.stem}/{p_download.name}"
-        )
+        if weatherfile_country in ["DZA", "EGY", "ETH", "GHA", "KEN", "LBY", "MAR", "MDG", "SEN", "TUN", "ZAF", "ZWE"]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/africa_wmo_region_1/"
+                f"{weatherfile_country}/{p_download.stem}/{p_download.name}"
+            )
+        elif weatherfile_country in [
+            "ARE",
+            "BGD",
+            "CHN",
+            "IND",
+            "JPN",
+            "KAZ",
+            "KOR",
+            "KWT",
+            "LKA",
+            "MAC",
+            "MDC",
+            "MNG",
+            "NPL",
+            "PAK",
+            "PRK",
+            "RUS",
+            "SAU",
+            "THA",
+            "TWN",
+            "UZB",
+            "VNM",
+        ]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/asia_wmo_region_2/"
+                f"{weatherfile_country}/{p_download.stem}/{p_download.name}"
+            )
+        elif weatherfile_country in ["ARG", "BOL", "BRA", "CHL", "COL", "ECU", "PER", "PRY", "URY", "VEN"]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/south_america_wmo_region_3/"
+                f"{weatherfile_country}/{p_download.stem}/{p_download.name}"
+            )
+        elif weatherfile_country in [
+            "BLZ",
+            "CAN",
+            "CUB",
+            "GTM",
+            "HND",
+            "MEX",
+            "MTQ",
+            "NIC",
+            "PRI",
+            "SLV",
+            "USA",
+            "VIR",
+        ]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/north_and_central_america_wmo_region_4/"
+                f"{weatherfile_country}/{weatherfile_state}/{p_download.stem}/{p_download.name}"
+            )
+        elif weatherfile_country in ["AUS", "BRN", "FJI", "GUM", "MHL", "MYS", "NZL", "PHL", "PLW", "SGP", "UMI"]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/southwest_pacific_wmo_region_5/"
+                f"{weatherfile_country}/{p_download.stem}/{p_download.name}"
+            )
+        elif weatherfile_country in [
+            "AUT",
+            "BEL",
+            "BGR",
+            "BIH",
+            "BLR",
+            "CHE",
+            "CYP",
+            "CZE",
+            "DEU",
+            "DNK",
+            "ESP",
+            "FIN",
+            "FRA",
+            "GBR",
+            "GRC",
+            "HRV",
+            "HUN",
+            "IRL",
+            "ISL",
+            "ISR",
+            "ITA",
+            "LTU",
+            "NLD",
+            "NOR",
+            "POL",
+            "PRT",
+            "ROU",
+            "RUS",
+            "SRB",
+            "SVK",
+            "SVN",
+            "SWE",
+            "SYR",
+            "TUR",
+            "UKR",
+        ]:
+            weatherfile_url = (
+                "https://energyplus-weather.s3.amazonaws.com/europe_wmo_region_6/"
+                f"{weatherfile_country}/{p_download.stem}/{p_download.name}"
+            )
+        else:
+            raise ValueError(f"Unsupported country: {weatherfile_country}")
+
         outputname = p_save / p_download.name
         logger.debug(f"Downloading weather file from {weatherfile_url}")
         try:
@@ -225,11 +324,7 @@ class SystemParameters:
                     f"Returned non 200 status code trying to download weather file: {weatherfile_data.status_code}"
                 )
         except requests.exceptions.RequestException as e:
-            raise requests.exceptions.RequestException(
-                f"Could not download weather file: {weatherfile_url}"
-                "\nAt this time we only support USA weather stations"
-                f"\n{e}"
-            )
+            raise requests.exceptions.RequestException(f"Could not download weather file: {weatherfile_url}" f"\n{e}")
 
         if not outputname.exists():
             raise FileNotFoundError(f"Could not find or download weather file for {p_download!s}")
