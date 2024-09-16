615031242d787b879dcea33a58925302476b490b
Nathan Moore <nathan.moore@nrel.gov>
2024-09-16 14:34:34 -0600

    only simulate multi-ghe with Dymola, temporarily

--- tests/model_connectors/test_district_multi_ghe.py
+++ tests/model_connectors/test_district_multi_ghe.py
@@ -105,7 +105,10 @@ class DistrictSystemTest(TestCaseBase):
         root_path = Path(self.district._scaffold.districts_path.files_dir).resolve()
         assert (root_path / "DistrictEnergySystem.mo").exists()
 
-    @pytest.mark.simulation
+    # @pytest.mark.simulation
+    # @pytest.mark.skip(reason="Model is too large for OM. Simulates as expected using Dymola")
+    @pytest.mark.dymola
+    # TODO: Improve simulation with OM to enable running this test natively
     def test_simulate_multi_ghe_district(self):
         self.run_and_assert_in_docker(
             f"{self.district._scaffold.project_name}.Districts.DistrictEnergySystem",
