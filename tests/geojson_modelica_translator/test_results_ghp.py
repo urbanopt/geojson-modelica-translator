cf4ab1cdee77c121315db07e9d98436d84262388
tanushree04 <tanushree.charan@nrel.gov>
2024-09-22 19:13:57 -0600

    add multiple ghp test

--- tests/geojson_modelica_translator/test_results_ghp.py
+++ tests/geojson_modelica_translator/test_results_ghp.py
@@ -19,15 +19,19 @@ class ResultsTest(unittest.TestCase):
         modelica_path = Path(self.data_dir) / "modelica_5"
         modelica_path = modelica_path.resolve()
 
-        result = ResultsModelica(modelica_path)
-        result.calculate_results()
-
         # Construct the path to the CSV file
         csv_file_path = (
             modelica_path
             / "modelica_5.Districts.DistrictEnergySystem_results"
             / "modelica_5.Districts.DistrictEnergySystem_result.csv"
         )
+        
+        # Delete csv path if it exists
+        if csv_file_path.exists():
+            csv_file_path.unlink()
+
+        result = ResultsModelica(modelica_path)
+        result.calculate_results()
 
         # Check if the CSV file exists
         assert csv_file_path.exists(), f"File does not exist at path: {csv_file_path}"
@@ -40,5 +44,49 @@ class ResultsTest(unittest.TestCase):
         assert "heating_electric_power_d55aa383" in df.columns, "The heating_electric_power column is missing from the CSV file."
 
         assert "pump_power_3da62a1d" in df.columns, "The pump_power column is missing from the CSV file."
+
+        assert "electrical_power_consumed" in df.columns, "The electrical_power_consumed column is missing from the CSV file."
+
+    def test_result_multiple_ghp(self):
+
+        # Construct the path to the Modelica project directory
+        modelica_path = Path(self.data_dir) / "modelica_multiple"
+        modelica_path = modelica_path.resolve()
+
+        # Construct the path to the CSV file
+        csv_file_path = (
+            modelica_path
+            / "modelica_multiple.Districts.DistrictEnergySystem_results"
+            / "modelica_multiple.Districts.DistrictEnergySystem_result.csv"
+        )
+
+        # Delete csv path if it exists
+        if csv_file_path.exists():
+            csv_file_path.unlink()
+
+        result = ResultsModelica(modelica_path)
+        result.calculate_results()
+
+        # Check if the CSV file exists
+        assert csv_file_path.exists(), f"File does not exist at path: {csv_file_path}"
+
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(csv_file_path)
+
+        assert "Datetime" in df.columns, "The 'Datetime' column is missing from the CSV file."
+
+        # Check if any columns contain the "heating_electric_power_" substring
+        heating_electric_power = [col for col in df.columns if "heating_electric_power_" in col]
+
+        assert heating_electric_power, "No columns with 'heating_electric_power' found in the CSV file."
+
+        assert len(heating_electric_power) == 13, f"Expected 13 columns with 'heating_electric_power_' but found {len(heating_electric_power)}. Missing or extra columns in the CSV file."
+
+        pump_power = [col for col in df.columns if "pump_power_" in col]
+
+        # Assert that there is at least one column with the substring
+        assert pump_power, "No columns with 'pump_power' found in the CSV file."
+
+        assert len(pump_power) == 26, f"Expected 26 columns with 'pump_power' but found {len(pump_power)}. Missing or extra columns in the CSV file."
 
         assert "electrical_power_consumed" in df.columns, "The electrical_power_consumed column is missing from the CSV file."
