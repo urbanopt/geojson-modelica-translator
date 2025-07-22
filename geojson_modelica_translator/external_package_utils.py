e6b49e49f355b68c9ffa9834154dec2dd403276e
Jing Wang <jwang5@nrel.gov>
2025-07-22 17:17:32 -0600

    new function for getting the first building group in loop

--- geojson_modelica_translator/external_package_utils.py
+++ geojson_modelica_translator/external_package_utils.py
@@ -14,3 +14,14 @@ def load_loop_order(system_parameters_file: Path) -> list:
     if not loop_order_path.is_file():
         raise FileNotFoundError(f"Loop order file not found at {loop_order_path}")
     return json.loads(loop_order_path.read_text())
+
+def loop_order_first_index(loop_order: list) -> int:
+    """Find the first index of a building group in the loop order list
+
+    :param loop_order: list of building & ghe ids in loop order
+    :return: index of the first building group in the loop order
+    """
+    for i, obj in enumerate(loop_order):
+        if obj["type"] == "building group":
+            return i
+    raise ValueError("No building group found in loop_order")
